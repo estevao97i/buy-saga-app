02b63259b116f7544112b45497fe2ace8175c35e
estevo <pedro.cordova2002@gmail.com>
2023-09-05 00:17:24 -0300

    finished pedido choreography

--- buy-choreography/src/main/java/org/estevao/PedidoService.java
+++ buy-choreography/src/main/java/org/estevao/PedidoService.java
@@ -2,6 +2,19 @@ package org.estevao;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.Set;
+
 @ApplicationScoped
 public class PedidoService {
+
+    public Set<Long> id;
+
+    public void newPedido(Long id) {
+        this.id.add(id);
+    }
+
+    public void removePedido(Long id) {
+        this.id.remove(id);
+    }
+
 }
